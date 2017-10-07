553c67abb269bea7963816abca71a614e2a711a9
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-10-07 23:48:23 +0900

    [Add] tcpアクセスを中継するtcpサーバー追加

---
+++ index.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const net = require('net');
+const {
+    parse: parseUrl,
+ } = require('url');
+ 
+assert(process.argv[2], 'remote address("hostname:port") required.');
+
+const {
+    hostname: REMOTE_HOSTNAME,
+    port: REMOTE_PORT,
+} = parseUrl(`http://${process.argv[2] || 'localhost'}`);
+
+assert(REMOTE_PORT, 'remote port number required.');
+
+const LOCAL_PORT = Number(process.argv[3]) || 0;
+
+const tcpProxyServer = net.createServer();
+
+tcpProxyServer.on('connection', (clientSocket) => {
+    const serverSocket = net.createConnection(REMOTE_PORT, REMOTE_HOSTNAME);
+    serverSocket.once('connect', () => {
+        clientSocket.pipe(serverSocket);
+        serverSocket.pipe(clientSocket);
+    });
+    serverSocket.on('error', err => {
+        clientSocket.end(err.message);
+    });
+    clientSocket.on('error', () => {
+        serverSocket.destroy();
+    });
+});
+
+tcpProxyServer.on('listening', () => {
+    console.info(`TCP server started. (IP:port = 0.0.0.0:${tcpProxyServer.address().port})`);
+    console.info(`transport to ${REMOTE_HOSTNAME}:${REMOTE_PORT}`);
+});
+
+tcpProxyServer.listen(LOCAL_PORT);
